58f9b43ffb1e5a4e4e6566263d7ecc68371c4d72
nguyenhuukhanh-25-05-05 <nguyenhuukhanh250505@gmail.com>
2026-07-02 10:00:00 +0700

    feat: assemble landing page view

---
+++ src/views/LandingPageView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HeaderNav from '../components/HeaderNav.vue'
+import HeroSection from '../components/HeroSection.vue'
+import PressBar from '../components/PressBar.vue'
+import CategorySection from '../components/CategorySection.vue'
+import ProductSection from '../components/ProductSection.vue'
+import EditorialSection from '../components/EditorialSection.vue'
+import ProcessSection from '../components/ProcessSection.vue'
+import TestimonialSection from '../components/TestimonialSection.vue'
+import GallerySection from '../components/GallerySection.vue'
+import CtaBanner from '../components/CtaBanner.vue'
+import FooterSection from '../components/FooterSection.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-white font-['Plus_Jakarta_Sans',sans-serif] antialiased overflow-x-hidden">
+    <HeaderNav />
+    <main>
+      <HeroSection />
+      <PressBar />
+      <CategorySection />
+      <ProductSection />
+      <EditorialSection />
+      <ProcessSection />
+      <TestimonialSection />
+      <GallerySection />
+      <CtaBanner />
+    </main>
+    <FooterSection />
+  </div>
+</template>
